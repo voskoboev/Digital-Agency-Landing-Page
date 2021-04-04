cdd5827fd46d0fc8e346a2138710e28a61675733
Dmitry <dvoskoboev@gmail.com>
2021-04-04 20:18:01 +0300

    added order section and half of footer

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// order__form-input--service
+
+// const input = document.querySelector('.order__form-input--service');
+// const v123 = document.querySelector('.123');
+
+// input.onclick(() => {
+  
+// });
